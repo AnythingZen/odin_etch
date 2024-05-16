4da18ea2606dc61a8127c2e5a163da507ca81c7f
anythingzen <chengzhanxd@gmail.com>
2024-05-16 14:38:32 +0800

    Made it more dynamic with flex rather than calculating the height and width for each square statically. Moved styling over to css instead as well.

--- main.js
+++ main.js
@@ -2,30 +2,24 @@ const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset');
 let numOfSquares = 16;
 
-// make square divs based on num^2
+// make square via rows and columns
 function makeSquare (numOfSquares) {
 
-    for (let i = 0; i < numOfSquares**2; i++) {
-        const squares = document.createElement('div');
-        squares.classList.add('square');
-        container.appendChild(squares);
+    for (let i = 0; i < numOfSquares; i++) {
+        const rows = document.createElement('div');
+        rows.classList.add('rows');
+        container.appendChild(rows);
+
+        // add square to each row
+        for (let i =0; i< numOfSquares; i++) {
+            const squares = document.createElement('div');
+            squares.classList.add('squares');
+            rows.appendChild(squares);
+        }
     }
 }
 
 makeSquare(numOfSquares);
-
-// style it with flex 
-function style () {
-    const squares = document.querySelectorAll('.square');
-    const squareWidth = 959/numOfSquares;
-    const squareHeight = 959/numOfSquares;
-    squares.forEach((square) => {
-        square.style.width = `${squareWidth}px`;
-        square.style.height = `${squareHeight}px`;
-    });
-}
-
-style();
 
 // made for when hover, color changes on square
 function changeColor (eachSquare) {
@@ -36,7 +30,7 @@ function changeColor (eachSquare) {
 container.addEventListener('mouseover', (event) => {
     const target = event.target;
 
-    if (target.classList.contains('square')) {
+    if (target.classList.contains('squares')) {
         changeColor(target);
     }
 });
@@ -51,6 +45,5 @@ resetButton.addEventListener('click', () => {
 
     container.innerHTML = '';
     makeSquare(numOfSquares);
-    style();
 });
 
